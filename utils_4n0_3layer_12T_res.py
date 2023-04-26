e086aacd1abb0d3f3ccefd68600e2bbdd44671d2
tianshapojun <383033930@qq.com>
2023-04-26 15:22:13 +0800

    Update utils_4n0_3layer_12T_res.py

--- utils_4n0_3layer_12T_res.py
+++ utils_4n0_3layer_12T_res.py
@@ -40,11 +40,12 @@ def construct_model(config,new=0):
 
     data = mx.sym.var("data")
     label = mx.sym.var("label")
+    #delete .tolist()
     adj = mx.sym.Variable('adj', shape=adj_mx.shape,
-                          init=mx.init.Constant(value=adj_mx.tolist()))
+                          init=mx.init.Constant(value=adj_mx))
     adj = mx.sym.BlockGrad(adj)
     mask_init_value = mx.init.Constant(value=(adj_mx != 0)
-                                       .astype('float32').tolist())
+                                       .astype('float32'))
 
     filters = config['filters']
     first_layer_embedding_size = config['first_layer_embedding_size']
